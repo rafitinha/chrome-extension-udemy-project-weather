2c9c03ab84c05ea37ef8e5940e4d311def6d9938
Arrombado <rafaoc7232@hotmail.com>
2023-04-01 22:43:16 -0300

    Start and Setup Material UI - 50

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,17 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./popup.css";
-import { fetchOpenWeatherData } from "../utils/api";
+import WeatherCard from "./WeatherCard";
 
 const App: React.FC<{}> = () => {
-  useEffect(() => {
-    fetchOpenWeatherData("Toronto")
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
-  }, []);
   return (
     <div>
-      <img src="icon.png" />
+      <WeatherCard city="Toronto" />
     </div>
   );
 };
